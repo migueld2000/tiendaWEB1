7317506a21d6cb7d5727fbeb7d4b6dd33d113536
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2020-11-14 07:17:48 -0500

    ProductosBodega.php

--- ProductosBodega.php
+++ ProductosBodega.php
@@ -4,255 +4,46 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PRODUCTOS</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+    <title>PRODUCTOS BODEGA</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
 </head>
 
 <body>
     <main>
-        <div class="container">
-            <form action="registrarbodega.php" method="POST">
-                <h1>Productos de la bodega</h1>
-                <hr />
-                <div class="row">
-                    <div class="col">
-                        <img src="leche-colanta.jpg" alt="..." class="img-thumbnail"
-                            style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">LECHE COLANTA</h5>
-                                <p class="card-text">Precio:$1500</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar1">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar1">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <img src="quesito-colanta.jpg" alt="..." class="img-thumbnail"
-                            style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">QUESITOCOLANTA</h5>
-                                <p class="card-text">Precio:$1500</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar2">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar2">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <img src="iuy1uv1j.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">HUEVOS</h5>
-                                <p class="card-text">Precio:$1500</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar3">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar3">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <img src="Margarina-Rama.jpg" alt="..." class="img-thumbnail"
-                            style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">MANTEQUILLA RAMA</h5>
-                                <p class="card-text">Precio:$1500</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar4">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar4">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="w-100"></div>
+    <?php
+        //1. crear una copia de la clase Bd
+        //crear un objeto  de la clase bd
+        include("Basededatos.php");
+        $transaccion = new Basededatos();
 
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="97m5924e.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">TOMATES</h5>
-                                <p class="card-text">Precio:$2000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar5">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar5">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="26yc1x1n.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">CEBOLLA DE HUEVO</h5>
-                                <p class="card-text">Precio:$2000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar6">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar6">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="nsbg6imw.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">CEBOLLA DE RAMA</h5>
-                                <p class="card-text">Precio:$2000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar7">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar7">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="orozx172.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">PAPAS</h5>
-                                <p class="card-text">Precio:$2500</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar8">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar8">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="w-100"></div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="bfzl07b0.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">CARNE DE RES</h5>
-                                <p class="card-text">Precio:$3000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar9">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar9">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="j9qv1713.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">CARNE DE CERDO</h5>
-                                <p class="card-text">Precio:$3000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar10">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar10">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="tyywf8s6.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">POLLO</h5>
-                                <p class="card-text">Precio:$3000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar11">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar11">
-                                    Borrar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg">
-                        <br />
-                        <br />
-                        <img src="irs57vd0.jpg" alt="..." class="img-thumbnail" style="width: 400px; height: 230px" />
-                        <br />
-                        <br />
-                        <div class="card" style="width: 255px">
-                            <div class="card-body">
-                                <h5 class="card-title">PEZCADO</h5>
-                                <p class="card-text">Precio:$3000</p>
-                                <button type="submit" class="btn btn-info btn-block" name="botonEditar12">
-                                    Editar
-                                </button>
-                                <button type="submit" class="btn btn-info btn-block" name="botonBorrar12">
-                                    Borrar
-                                </button>
-                            </div>
+        //2.Crear la consulta para buscar datos
+        $consultaSQL = "SELECT * FROM productos WHERE 1";
+
+        //3.Utilizar el metodo consultarDatos
+        $productos = $transaccion->consultarDatos($consultaSQL);
+        print_r($productos)
+    ?>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3">
+                <div class="col mb-4">
+                    <?php foreach($productos as $producto): ?>
+                    <div class="card">
+                        <img src="<?php echo($producto["Imagen"])?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php  echo($producto["Nombre"])?></h5>
+                            <p class="card-text"><?php echo($producto["Marca"]) ?></p>
+                            <p class="card-text"><?php echo($producto["Precio"]) ?></p>
+                            <p class="card-text"><?php echo($producto["Descripcion"]) ?></p>
+                        <a href="eliminarproducto.php?id=<?php echo($producto["ID_producto"])?>"class="btn btn-danger">Eliminar</a>
                         </div>
                     </div>
                 </div>
-                <br />
-            </form>
+                <?php endforeach ?>
+            </div>
         </div>
     </main>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 
 </html>
