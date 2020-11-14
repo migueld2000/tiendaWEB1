750b310a3343017980f1fc4753a5d56f75e84f03
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2020-11-14 07:16:17 -0500

    eliminarproducto.php

---
+++ eliminarproducto.php
@@ -0,0 +1,14 @@
+<?php
+//1. incliur el archivo base de datos
+include("Basededatos.php");
+//2.Reciba el Id a eliminar
+$id=$_GET["id"];
+//3. Crear un objeto(sacar copia) de la clase Base de datos
+$transaccion=new Basededatos();
+//4. crear una consulta para eliminar datos
+$consultaSQl="DELETE FROM productos WHERE ID_producto='$id'";
+
+//5.ejecutar el metodo eliminarUsuarios
+$transaccion->eliminarDatos($consultaSQl);
+
+?>
