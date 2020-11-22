453f026a0c566d01d70d56025ba74910d298c950
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2020-11-22 12:23:45 -0500

    ProductosBodega.php

--- ProductosBodega.php
+++ ProductosBodega.php
@@ -10,7 +10,7 @@
 
 <body>
     <main>
-    <?php
+        <?php
         //1. crear una copia de la clase Bd
         //crear un objeto  de la clase bd
         include("Basededatos.php");
@@ -21,24 +21,79 @@
 
         //3.Utilizar el metodo consultarDatos
         $productos = $transaccion->consultarDatos($consultaSQL);
-        print_r($productos)
-    ?>
+        ?>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Menu</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Productos.php">Productos<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="ProductosBodega.php">Bodega</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <div class="container">
             <div class="row row-cols-1 row-cols-md-3">
                 <div class="col mb-4">
-                    <?php foreach($productos as $producto): ?>
-                    <div class="card">
-                        <img src="<?php echo($producto["Imagen"])?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php  echo($producto["Nombre"])?></h5>
-                            <p class="card-text"><?php echo($producto["Marca"]) ?></p>
-                            <p class="card-text"><?php echo($producto["Precio"]) ?></p>
-                            <p class="card-text"><?php echo($producto["Descripcion"]) ?></p>
-                        <a href="eliminarproducto.php?id=<?php echo($producto["ID_producto"])?>"class="btn btn-danger">Eliminar</a>
+                    <?php foreach ($productos as $producto) : ?>
+                        <div class="card h-100">
+                            <img src="<?php echo ($producto["Imagen"]) ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo ($producto["Nombre"]) ?></h5>
+                                <p class="card-text"><?php echo ($producto["Marca"]) ?></p>
+                                <p class="card-text"><?php echo ($producto["Precio"]) ?></p>
+                                <p class="card-text"><?php echo ($producto["Descripcion"]) ?></p>
+                                <a href="eliminarproducto.php?id=<?php echo ($producto["ID_producto"]) ?>" class="btn btn-danger">Eliminar</a>
+                                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#editar<?php echo ($producto["ID_producto"]) ?>">
+                                    Editar
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                        <div class="modal fade" id="editar<?php echo ($producto["ID_producto"]) ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edicion del producto</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form action="EditarProductos.php?id=<?php echo ($producto["ID_producto"]) ?>" method="POST">
+                  <div class="form-group">
+                    <label>Foto</label>
+                    <input type="text" class="form-control" placeholder="Nombre" name="EditarFoto" value="<?php echo ($producto["Imagen"]) ?>">
+                  </div>
+                  <div class="form-group">
+                    <label>Nombre del producto</label>
+                    <input type="text" class="form-control" placeholder="Nombre" name="EditarNombre" value="<?php echo ($producto["Nombre"]) ?>">
+                  </div>
+                  <div class="form-group">
+                    <label>Nombre del producto</label>
+                    <input type="text" class="form-control" placeholder="Nombre" name="EditarMarca" value="<?php echo ($producto["Marca"]) ?>">
+                  </div>
+                  <div class="form-group">
+                    <label>Nombre del producto</label>
+                    <input type="text" class="form-control" placeholder="Nombre" name="EditarPrecio" value="<?php echo ($producto["Precio"]) ?>">
+                  </div>
+                  <div class="form-group">
+                    <label>Descripcion</label>
+                    <textarea class="form-control" name="EditarDescripcion" rows="3"><?php echo ($producto["Descripcion"]) ?></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-info" name="Editar">Editar</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
                 </div>
-                <?php endforeach ?>
+            <?php endforeach ?>
             </div>
         </div>
     </main>
