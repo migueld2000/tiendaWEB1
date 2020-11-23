51da40e9aafbfd1f6989ff2bc7618f9229d0a2f1
migueld2000 <70757824+migueld2000@users.noreply.github.com>
2020-11-23 16:55:58 -0500

    ProductosBodega.php

--- ProductosBodega.php
+++ ProductosBodega.php
@@ -42,10 +42,10 @@
     <br>
     <div class="container">
       <div class="row row-cols-1 row-cols-md-3">
-        <div class="col mb-4">
-          <?php foreach ($productos as $producto) : ?>
+      <?php foreach ($productos as $producto) : ?>
+        <div class="col-md-4">
             <div class="card h-100">
-              <img src="<?php echo ($producto["Imagen"]) ?>" class="card-img-top" alt="...">
+              <img src="<?php echo ($producto["Imagen"]) ?>" class="card-img-top img-thumbnail">
               <div class="card-body">
                 <h5 class="card-title"><?php echo ($producto["Nombre"]) ?></h5>
                 <p class="card-text"><?php echo ($producto["Marca"]) ?></p>
@@ -102,5 +102,4 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
-
 </html>
